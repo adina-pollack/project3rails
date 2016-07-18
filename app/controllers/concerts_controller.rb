0866de380bb3bda08421f33b31be22c68c5d12fc
Adina Pollack <adinap16@gmail.com>
2016-07-18 11:54:52 -0400

    CRUD for concerts

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,37 @@
 class ConcertsController < ApplicationController
+  def index
+    @concerts = Concert.all
+  end
+  def show
+    @concert = Concert.find(params[:id])
+  end
+  def new
+    @venue = Venue.find(params[:venue_id])
+    @concert = @venue.concerts.new
+  end
+  def create
+    @venue = Venue.find(params[:venue_id])
+    @concert = Concert.create!(concert_params.merge(venue: @venue))
+    redirect_to @venue
+  end
+  def edit
+    @concert = Concert.find(params[:id])
+    @venue = Venue.find(params[:venue_id])
+  end
+  def update
+    @venue = Venue.find(params[:venue_id])
+    @concert = Concert.find(params[:id])
+    @concert.update(concert_params.merge(venue: @venue))
+    redirect_to venue_concert_path(@venue, @concert)
+  end
+  def destroy
+    @venue = Venue.find(params[:venue_id])
+    @concert = @venue.concerts.find(params[:id])
+    @concert.destroy
+    redirect_to @venue
+  end
+  private
+  def concert_params
+    params.require(:concert).permit(:name, :datetime, :ticket_status)
+  end
 end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -5,4 +5,15 @@ class VenuesController < ApplicationController
   def show
     @venue = Venue.find(params[:id])
   end
+  def new
+    @venue = Venue.new
+  end
+  def create
+    @venue = Venue.create!(venue_params)
+    redirect_to venues_path
+  end
+  private
+  def venue_params
+    params.require(:venue).permit(:name, :city)
+  end
 end
